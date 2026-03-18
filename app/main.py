d04a066e459e725e9d69e9e5096f198a350633c5
Souvik <sou.90kd@gmail.com>
2026-03-19 00:47:49 +0530

    Add Vercel URLs to CORS allowed origins

--- app/main.py
+++ app/main.py
@@ -10,7 +10,9 @@ app = FastAPI(title=settings.app_name)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+    "https://nudge-repo.vercel.app",
+    "https://nudge-repo-hknkrsu3t-souvikkais-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
